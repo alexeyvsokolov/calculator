2d8f0b3e523365b204768c6cbfcd05536b4b2792
alexeyvsokolov <alexeysokolovgeneral@gmail.com>
2022-05-22 14:48:42 +0300

    add difference operation, button -

--- script.js
+++ script.js
@@ -56,9 +56,21 @@ document.getElementById('btn_sum').addEventListener('click', function () {
     inputWindow.value = '';
 })
 
+document.getElementById('btn_def').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'def';
+    inputWindow.value = '';
+})
+
 document.getElementById('btn_calc').addEventListener('click', function () {
     if (operation === 'sum'){
         const result = lastOperand + parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'def'){
+        const result = lastOperand - parseInt(inputWindow.value);
         operation = null;
         lastOperand = 0;
         inputWindow.value = result;
